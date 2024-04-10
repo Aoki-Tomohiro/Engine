0d2928796d0f552b0d4f40ef7d85d0b253162719
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-04-10 14:40:21 +0900

    AL4_Ultimate1

--- Project/Engine/Components/Collision/CollisionConfig.h
+++ Project/Engine/Components/Collision/CollisionConfig.h
@@ -2,20 +2,18 @@
 #include <cstdint>
 
 //CollisionAttribute
-const uint32_t kCollisionAttributePlayer = 0b00001;
-const uint32_t kCollisionAttributeEnemy = 0b00010;
-const uint32_t kCollisionAttributeWeapon = 0b00100;
-const uint32_t kCollisionAttributeMissile = 0b01000;
-const uint32_t kCollisionAttributeLaser = 0b10000;
+const uint32_t kCollisionAttributePlayer = 0b0001;
+const uint32_t kCollisionAttributeEnemy = 0b0010;
+const uint32_t kCollisionAttributeFloor = 0b0100;
+const uint32_t kCollisionAttributeGoal = 0b1000;
 
 //CollisionMask
-const uint32_t kCollisionMaskPlayer = 0b11010;
-const uint32_t kCollisionMaskEnemy = 0b00101;
-const uint32_t kCollisionMaskWeapon = 0b01010;
-const uint32_t kCollisionMaskMissile = 0b00101;
-const uint32_t kCollisionMaskLaser = 0b00001;
+const uint32_t kCollisionMaskPlayer = 0b1110;
+const uint32_t kCollisionMaskEnemy = 0b1101;
+const uint32_t kCollisionMaskFloor = 0b1011;
+const uint32_t kCollisionMaskGoal = 0b0001;
 
 //形状
 const uint32_t kCollisionPrimitiveSphere = 0b1;
-const uint32_t kCollisionPrimitiveAABB = kCollisionPrimitiveSphere << 1;
+const uint32_t kCollisionPrimitiveAABB = 0b1 << 1;
 const uint32_t kCollisionPrimitiveOBB = kCollisionPrimitiveAABB << 1;
