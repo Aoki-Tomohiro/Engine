980375227c8f2bb5b8f3d90fb2fca25c658e4cd9
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-04-10 12:08:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Project/Application/Src/Scene/SceneFactory.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Engine/Framework/Scene/AbstractSceneFactory.h"
+#include "GameTitleScene.h"
+#include "GamePlayScene.h"
+#include "GameClearScene.h"
+#include "GameOverScene.h"
+#include "LoadScene.h"
+
+class SceneFactory : public AbstractSceneFactory
+{
+public:
+	IScene* CreateScene(const std::string& sceneName) override;
+};
+
