d9871dcd4d95f9d787d9939d714b8a024bc4ee62
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-04-12 14:48:43 +0900

    完成

--- Project/Application/Src/Scene/GameTitleScene.h
+++ Project/Application/Src/Scene/GameTitleScene.h
@@ -1,10 +1,8 @@
 #pragma once
 #include "Engine/Framework/Scene/IScene.h"
 #include "Engine/Base/Renderer.h"
-#include "Engine/Base/ImGuiManager.h"
 #include "Engine/Components/Input/Input.h"
 #include "Engine/Components/Audio/Audio.h"
-#include "Engine/3D/Model/ModelManager.h"
 
 class GameTitleScene : public IScene
 {
@@ -25,11 +23,5 @@ private:
 	Input* input_ = nullptr;
 
 	Audio* audio_ = nullptr;
-
-	Camera camera_{};
-
-	WorldTransform worldTransform_{};
-
-	std::unique_ptr<Model> model_ = nullptr;
 };
 
