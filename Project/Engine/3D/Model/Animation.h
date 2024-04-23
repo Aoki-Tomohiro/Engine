e06db3e2acdea5c352ad98a2d7ef164b73ae2325
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-04-24 04:32:40 +0900

    CG4_00

--- Project/Engine/3D/Model/Animation.h
+++ Project/Engine/3D/Model/Animation.h
@@ -1,5 +1,59 @@
 #pragma once
+#include <map>
+#include <string>
+#include <vector>
+#include "Engine/Math/Vector3.h"
+#include "Engine/Math/Quaternion.h"
+#include "Engine/Math/Matrix4x4.h"
+
 class Animation
 {
+public:
+	//Keyframe構造体
+	template <typename tValue>
+	struct Keyframe {
+		float time;
+		tValue value;
+	};
+	using KeyframeVector3 = Keyframe<Vector3>;
+	using KeyframeQuaternion = Keyframe<Quaternion>;
+
+	//AnimationCurve構造体
+	template <typename tValue>
+	struct AnimationCurve {
+		std::vector<Keyframe<tValue>> keyframes;
+	};
+
+	//NodeAnimation構造体
+	struct NodeAnimation {
+		AnimationCurve<Vector3> translate;
+		AnimationCurve<Quaternion> rotate;
+		AnimationCurve<Vector3> scale;
+	};
+
+	//Animation構造体
+	struct AnimationData {
+		float duration;//アニメーション全体の尺(単位は秒)
+		//NodeAnimationの集合。Node名でひけるようにしておく
+		std::map<std::string, NodeAnimation> nodeAnimations;
+	};
+
+	void Initialize(const AnimationData& animationData);
+
+	void Update(const std::string& name);
+
+	const Matrix4x4& GetLocalMatrix() const { return localMatrix_; };
+
+private:
+	Vector3 CalculateValue(const std::vector<KeyframeVector3>& keyframes, float time);
+
+	Quaternion CalculateValue(const std::vector<KeyframeQuaternion>& keyframes, float time);
+
+private:
+	AnimationData animationData_{};
+
+	Matrix4x4 localMatrix_{};
+
+	float animationTime_ = 0.0f;
 };
 
--- Project/Engine/3D/Model/Model.cpp
+++ Project/Engine/3D/Model/Model.cpp
@@ -3,7 +3,7 @@
 #include "Engine/Base/TextureManager.h"
 #include "Engine/Math/MathFunction.h"
 
-void Model::Create(const ModelData& modelData, DrawPass drawPass)
+void Model::Create(const ModelData& modelData, const Animation::AnimationData& animationData, DrawPass drawPass)
 {
 	//モデルデータを設定
 	modelData_ = modelData;
@@ -16,13 +16,20 @@ void Model::Create(const ModelData& modelData, DrawPass drawPass)
 	material_ = std::make_unique<Material>();
 	material_->Initialize(modelData_.material.textureFilePath);
 
+	//アニメーションの作成
+	animation_ = std::make_unique<Animation>();
+	animation_->Initialize(animationData);
+
 	//描画パスを設定
 	drawPass_ = drawPass;
 }
 
-void Model::Update()
+void Model::Update(WorldTransform& worldTransform)
 {
-	material_->Update();
+	//アニメーションの適用
+	animation_->Update(modelData_.rootNode.name);
+	worldTransform.matWorld_ = animation_->GetLocalMatrix() * worldTransform.matWorld_;
+	worldTransform.TransferMatrix();
 }
 
 void Model::Draw(WorldTransform& worldTransform, const Camera& camera)
@@ -30,6 +37,9 @@ void Model::Draw(WorldTransform& worldTransform, const Camera& camera)
 	//RootのMatrixを適用
 	worldTransform.matWorld_ *= modelData_.rootNode.localMatrix;
 	worldTransform.TransferMatrix();
+
+	//マテリアルの更新
+	material_->Update();
 
 	//レンダラーのインスタンスを取得
 	Renderer* renderer_ = Renderer::GetInstance();
